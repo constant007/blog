c36bbba9ab340ad79176b5204e8d42fab41ee9f0
constant007 <29997442@qq.com>
2017-06-06 17:53:41 +0800

    Update freq_analyse.m

--- freq_analyse.m
+++ freq_analyse.m
@@ -1,15 +1,22 @@
 %matab script for amplitude-freq analyse
-function [y_fft]=freq_analyse(data_in,fs)
+% y_fft: amplitude in dB normalized
+% y_power : power
+% y_power_db : power in dB
+function [y_fft,y_power,y_power_db,freq]=freq_analyse(data_in,fs)
 y_len = length(data_in);
 fft_data = fft(data_in);
 fft_data_abs = abs(fftshift(fft_data))/(y_len/2); % true amplitude
 
-y_fft = 10*log10(fft_data_abs/max(fft_data_abs)); %dB normalized
+y_fft = 10*log10(fft_data_abs/max(fft_data_abs)); %amplitude dB normalized
+freq = (-y_len/2:(y_len-1)/2)*fs;
 
-f_x = (-y_len/2:(y_len-1)/2)*fs;
+y_power=(fft_data_abs.^2)/2;
+y_power_db = 10*log10(y_power);
+
+
 figure
-plot(f_x,y_fft);
-xlabel('freq/Hz');
-ylabel('Amplitude/dB');
+plot(freq,y_fft);
+xlabel('freq(Hz)');
+ylabel('Amplitude(dB)');
 grid on;
 end
