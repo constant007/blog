77e94365a2f01665a1fe3cb2def97eff42f38d04
constant007 <29997442@qq.com>
2018-10-20 13:00:17 +0800

    Update sampleinterp.m

--- sampleinterp.m
+++ sampleinterp.m
@@ -6,7 +6,7 @@ N = 4096;
 fs = 10e6;
 f = 1e6;
 n = (0:N-1)/fs;
-interp = 3;
+interp = 4;
 
 xin= sin(2*pi*f*n);
 xinp2 = zeros(1,N*2);
@@ -21,13 +21,23 @@ yp2=fft(xinp2);
 yp2f = fftshift(abs(yp2/N/2));
 
 xf = (-N/2+1:N/2)*fs/N; 
+xfomiga = xf/fs; 
 plot(xf,yf,'b');
 title('Amplitude Spectrum of X(t)');
 xlabel('f (Hz)');
 ylabel('|P1(f)|');
 
 xp2f = (-N*interp/2+1:N*interp/2)*interp*fs/(N*interp); 
+xp2fomiga = xp2f/(fs*interp);%Normalize
 hold on;
 
 plot(xp2f,yp2f,'r');
+figure%Normalize
+title('Amplitude Spectrum of X(t)');
+xlabel('omiga');
+ylabel('|P1(f)|');
+plot(xfomiga,yf,'b');
+hold on;
+plot(xp2fomiga,yp2f,'r');
+
 
