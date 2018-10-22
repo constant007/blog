fb076cc4b8ba1484be9bb89243bdfae4e6123593
constant007 <29997442@qq.com>
2018-10-22 16:07:05 +0800

    Update sampleinterp.m

--- sampleinterp.m
+++ sampleinterp.m
@@ -1,10 +1,8 @@
-clc;
-clear all;
-close all;
+clc;clear all;close all;
 
 N = 4096;
 fs = 10e6;
-f = 1e6;
+f = 4e6;
 n = (0:N-1)/fs;
 interp = 4;
 
@@ -16,12 +14,16 @@ end
 
 y=fft(xin);
 yf = fftshift(abs(y/N));
+yfm = max(yf);
+
 
 yp2=fft(xinp2);
 yp2f = fftshift(abs(yp2/N/2));
+yp2fm = max(yp2f);
 
 xf = (-N/2+1:N/2)*fs/N; 
 xfomiga = xf/fs; 
+%plot(xf,20*log10(yf/yfm),'b');
 plot(xf,yf,'b');
 title('Amplitude Spectrum of X(t)');
 xlabel('f (Hz)');
@@ -32,12 +34,27 @@ xp2fomiga = xp2f/(fs*interp);%Normalize
 hold on;
 
 plot(xp2f,yp2f,'r');
-figure%Normalize
+
+figure(2)%Normalize
+
+plot(xp2fomiga,yp2f,'r'); hold on;
 title('Amplitude Spectrum of X(t)');
-xlabel('omiga');
+xlabel('Normalized freq');
 ylabel('|P1(f)|');
 plot(xfomiga,yf,'b');
-hold on;
-plot(xp2fomiga,yp2f,'r');
 
+f = [0 0.8 0.8 1];
+mlo = [1 1 0 0];
+blo = fir2(34,f,mlo);
+yp2fir = filter(blo,1,xinp2);
+yp2firf =fft(yp2fir);
+yp2firf = fftshift(abs(yp2firf/N/2));
 
+figure(3)
+plot(xp2fomiga,yp2firf,'r');
+title('Amplitude Spectrum of X(t)');
+xlabel('Normalized freq');
+ylabel('|P1(f)|');
+
+figure(4)
+freqz(blo,1)  %filter freq %omiga freq
